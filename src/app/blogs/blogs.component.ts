51a22cad42a616317e9173b3bf5014c128ec8960
muhhammedSa3eed <muhhammedsa3eed013@gmail.com>
2025-07-05 03:23:14 +0300

    Update Thank You Page Ts

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-blogs',
+  standalone: true,
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './blogs.component.html',
   styleUrl: './blogs.component.css',
 })
-export class BlogsComponent {}
+export class BlogsComponent implements OnInit {
+  constructor(private viewportScroller: ViewportScroller) {}
+
+  ngOnInit(): void {
+    this.viewportScroller.scrollToPosition([0, 0]); // التمرير إلى الأعلى عند التهيئة
+  }
+}
--- src/app/thankyou/thankyou.component.ts
+++ src/app/thankyou/thankyou.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-thankyou',
+  standalone: true,
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './thankyou.component.html',
   styleUrl: './thankyou.component.css',
 })
-export class ThankyouComponent {}
+export class ThankyouComponent implements OnInit {
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+  }
+}
